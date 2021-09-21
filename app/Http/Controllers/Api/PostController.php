ba063f2d2ccc14dfc61f3d5fce7914f06e54e0a1
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-09-21 16:45:24 +0200

    first commit

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::paginate(6);
+
+        return response()->json([
+            'success' => true,
+            'results' => $posts
+        ]);
+    }
+
+    
+
+    
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        $post = Post::where('slug', $slug)->with(['category','tags'])->first();
+
+        if($post){
+            return response()->json([
+                'success' => true,
+                'results' => $post
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'results' => 'non ci sono dettagli'
+        ]);
+        
+    }
+}
+    
+
+    
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+            <router-link :to="{name: 'home'}" class="navbar-brand">
+                Boolpress
+            </router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <router-link :to="{name: 'home'}" class="nav-item nav-link">
+                        Home
+                    </router-link>
+                    <router-link :to="{name: 'about'}" class="nav-item nav-link">
+                        About
+                    </router-link>
+                    <router-link :to="{name: 'contacts'}" class="nav-item nav-link">
+                        Contacts
+                    </router-link>
+                    <router-link :to="{name: 'post'}" class="nav-item nav-link">
+                        Posts
+                    </router-link>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: "Header",
+    // components: {
+        
+    // }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
